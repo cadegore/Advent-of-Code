b45d4fb5eede71be9d9e929a86fd4fe2465d215f
Cade Gore <79346293+cadegore@users.noreply.github.com>
2023-12-01 18:20:03 -0500

    Advent of Code 2023 Day 1 - Part 2 Completed

--- 2023/12-01/main.js
+++ 2023/12-01/main.js
@@ -1,18 +1,43 @@
 const fs = require('fs');
-let sum = 0;
+let sum1 = 0;
+let sum2 = 0;
 
 fs.readFile('input.txt', 'utf-8', (error, string) => {
   string = string.split('\n');
   string.forEach((s) => {
-    s = Number(s.replace(/[^0-9]+/g, ''));
-    // console.log(s);
+    s = Number(s.replaceAll(/[^0-9]+/g, ''));
+    if (s.toString().length == 1) {
+      s = s * 10 + s;
+    } else if (s.toString.length != 2) {
+      s = Number(s.toString().split('').at(0) + s.toString().split('').at(-1));
+    }
+    sum1 += s;
+  });
+  console.log(`sum1 = ${sum1}`);
+
+  string.forEach((s) => {
+    string = s.split('\n');
+    s = Number(
+      s
+        .replaceAll('one', 'o1e')
+        .replaceAll('two', 't2o')
+        .replaceAll('three', 't3e')
+        .replaceAll('four', 'f4r')
+        .replaceAll('five', 'f5e')
+        .replaceAll('six', 's6x')
+        .replaceAll('seven', 's7n')
+        .replaceAll('eight', 'e8t')
+        .replaceAll('nine', 'n9e')
+        .replaceAll(/[^0-9]+/g, '')
+    );
 
     if (s.toString().length == 1) {
       s = s * 10 + s;
     } else if (s.toString.length != 2) {
       s = Number(s.toString().split('').at(0) + s.toString().split('').at(-1));
     }
-    sum += s;
+
+    sum2 += s;
   });
-  console.log(sum);
+  console.log(`sum2 = ${sum2}`);
 });
